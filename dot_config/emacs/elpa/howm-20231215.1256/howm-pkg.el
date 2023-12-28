c73d69be8419fd0fcbfbb474b2e756e1acd7f6a8
Chris Stott <chris@chrisstott.ca>
2023-12-27 19:16:12 -0700

    Update eza to eliminate quotes'

---
+++ dot_config/emacs/elpa/howm-20231215.1256/howm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "howm" "20231215.1256" "Wiki-like note-taking tool"
+  '((cl-lib "0.5"))
+  :commit "a11475a20527dcc38040200c86be0015ecfd36e9" :authors
+  '(("HIRAOKA Kazuyuki" . "kakkokakko@gmail.com"))
+  :maintainers
+  '(("HIRAOKA Kazuyuki" . "kakkokakko@gmail.com"))
+  :maintainer
+  '("HIRAOKA Kazuyuki" . "kakkokakko@gmail.com")
+  :url "https://kaorahi.github.io/howm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
